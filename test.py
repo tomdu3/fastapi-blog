c0c33aa1abece78a1b9a5c0544fec709437b1828
Tomislav Dukez <tomdu3@ymail.com>
2025-07-02 20:25:41 +0100

    add Annotated example to test without validation

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-def double(x: int) -> int:
+from typing import Annotated
+
+
+def double(x: Annotated[int, (0, 100)]) -> int:
     return x * 2
 
 
-result = double(4)
+result = double(111)
 print(result)
