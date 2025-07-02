e2b3b55645949ea19185d62a226d08b8ee0f118a
Tomislav Dukez <tomdu3@ymail.com>
2025-07-02 21:41:37 +0100

    add implementation of validation with typing

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-from typing import Annotated
+from typing import Annotated, get_type_hints, get_origin, get_args
 
 
 def double(x: Annotated[int, (0, 100)]) -> int:
-    return x * 2
+    type_hints = get_type_hints(double, include_extras=True)
+    hint = type_hints['x']
+    if get_origin(hint) is Annotated:
+        hint_type, *hint_args = get_args(hint)
+        low, high = hint_args[0]
+        print(hint_type)  # int
+        print(hint_args)  # value
+        print(low)  # 0
+        print(high)  # 100
+        
+        if low <= x <= high:
+            return x * 2
+        else:
+            raise ValueError(f"Value {x} is out of range [{low}, {high}]")
 
 
 result = double(111)
