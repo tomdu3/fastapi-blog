d19fe724d2429c50f3d8deae3dc025ea466526d9
Tomislav Dukez <tomdu3@ymail.com>
2025-07-03 17:41:09 +0100

    add db engine and session setup

---
+++ db.py
@@ -0,0 +1,13 @@
+from sqlmodel import create_engine, SQLModel, Session
+
+DATABASE_URL = 'sqlite:///db.sqlite'
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
