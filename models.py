da70f20e50e4a29696c1fd467d6d01625b51de39
Tomislav Dukez <tomdu3@ymail.com>
2025-07-03 17:40:13 +0100

    refactor schema into models sqlmodels

---
+++ models.py
@@ -0,0 +1,55 @@
+from enum import Enum
+from pydantic import BaseModel, field_validator
+from sqlmodel import SQLModel, Field, Relationship
+from datetime import date
+
+
+class GenreURLChoices(str, Enum):
+    rock = "rock"
+    grunge = "grunge"
+    alternative = "alternative"
+    progressive_rock = "progressive rock"
+
+
+class GenreChoices(str, Enum):
+    rock = "Rock"
+    grunge = "Grunge"
+    alternative = "Alternative"
+    progressive_rock = "Progressive Rock"
+
+
+class AlbumCreate(SQLModel):
+    title: str
+    release_date: date
+
+
+class AlbumBase(SQLModel):
+    title: str
+    release_date: date
+    band_id: int = Field(foreign_key="band.id")
+
+
+class Album(AlbumBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    band: "Band" = Relationship(back_populates="albums")
+
+
+class BandBase(SQLModel):
+    name: str
+    genre: GenreChoices
+
+
+class BandCreate(BandBase):
+    albums: list[AlbumCreate] | None = None
+
+    # Title case the genre and strip whitespace
+    @field_validator("genre", mode="before")
+    def title_case_genre(cls, v):
+        if isinstance(v, str):
+            return v.title().strip()
+        return v
+
+
+class Band(BandBase, table=True):
+    id: int = Field(default=None, primary_key=True)
+    albums: list[Album] = Relationship(back_populates="band")
